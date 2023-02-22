3482b065db982e3cd02d42e66000032d82b84ac0
Andrew Huynh <a5.t.huynh@gmail.com>
2023-02-21 20:27:30 -0800

    create-tauri-app w/ shortcuts

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,47 @@
+#![cfg_attr(
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
+)]
+use tauri::{App, AppHandle, GlobalShortcutManager, Manager};
+
+const SHORTCUT: &str = "Ctrl+Space";
+const WINDOW: &str = "launcher";
+
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+fn toggle_launchbar(app: &AppHandle) {
+    let window = app.get_window(WINDOW).expect("Did you label your window?");
+    if let Ok(true) = window.is_visible() {
+        let _ = window.hide();
+    } else {
+        let _ = window.show();
+    }
+}
+
+fn register_shortcut(app: &mut App) -> Result<(), tauri::Error> {
+    let app_handle = app.app_handle();
+    let mut shortcuts = app_handle.global_shortcut_manager();
+    if !shortcuts.is_registered(SHORTCUT)? {
+        shortcuts.register(SHORTCUT, move || toggle_launchbar(&app_handle))?;
+    }
+
+    Ok(())
+}
+
+fn main() {
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![greet])
+        .setup(move |app: &mut App| {
+            if let Err(err) = register_shortcut(app) {
+                eprintln!("Unable to register shortcut: {err}");
+            }
+
+            Ok(())
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
